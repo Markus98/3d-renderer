a135982d80e62100b530553b5cc48effd58c5545
Markus <Markus@MARKUS-PC>
2019-02-23 22:29:26 +0200

    Added Wall object that atm just creates the appropriate verticies for a wall.

--- src/vis/Entity.scala
+++ src/vis/Entity.scala
@@ -5,17 +5,22 @@ import scala.math
 
 case class Entity(pos: Pos) {
   var rot: Double = 0.0 // rotation in radians along z-axis
-  val verticies = Buffer[Pos]() // positions in relation to the entity's origin
+  val verticies = Buffer[Pos]() // Positions in relation to the entity's origin. The object's polygon is drawn in the same order as the buffer. 
 
-  // calculates a single point's location with z-axis rotation applied
+  /** Calculates a single point's location with z-axis rotation applied*/
   def rotPos(p: Pos) = Pos(p.x*math.cos(rot) - p.y*math.sin(rot), p.x*math.sin(rot) + p.y*math.cos(rot), p.z)
 
   def rotPositions = verticies.map(rotPos)
 
-  // vertex positions in global coordinate system
-  def vertexPositions = rotPositions.map(p => Pos(p.x + pos.x, p.y + pos.y, p.z + pos.z))
+  /** Vertex positions in global coordinate system*/
+  def vertexPositions = rotPositions.map( p => Pos(p.x + pos.x, p.y + pos.y, p.z + pos.z) )
 }
 
-/*class Wall extends Entity {
-  
-}*/
+class Wall(p: Pos, dir: Dir) extends Entity(p) {
+  this.rot = dir.rot
+  this.verticies += (Pos(-0.5,0,0), Pos(-0.5,0, Wall.Height), Pos(0.5,0,0), Pos(0.5,0, Wall.Height))
+}
+
+object Wall {
+  val Height = 2.0
+}
