c6926b0a93f9b88cb926dc5f4453193660e03e93
Markus <Markus@MARKUS-PC>
2019-02-23 23:40:08 +0200

    Shortened code with new Pos methods.

--- src/vis/Entity.scala
+++ src/vis/Entity.scala
@@ -6,11 +6,9 @@ import scala.math
 case class Entity(pos: Pos) {
   var rot: Double = 0.0 // rotation in radians along z-axis
   val verticies = Buffer[Pos]() // Positions in relation to the entity's origin. The object's polygon is drawn in the same order as the buffer. 
-
-  /** Calculates a single point's location with z-axis rotation applied*/
-  def rotPos(p: Pos) = Pos(p.x*math.cos(rot) - p.y*math.sin(rot), p.x*math.sin(rot) + p.y*math.cos(rot), p.z)
-
-  def rotPositions = verticies.map(rotPos)
+  
+  /** Returns the verticies of the entity with the rotation applied.*/
+  def rotPositions = verticies.map(_.rotated(rot))
 
   /** Vertex positions in global coordinate system*/
   def vertexPositions = rotPositions.map( p => Pos(p.x + pos.x, p.y + pos.y, p.z + pos.z) )
