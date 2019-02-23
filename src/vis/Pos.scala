5b50b586548d9120436b86525b1d3b0350e8a9a2
Markus <Markus@MARKUS-PC>
2019-02-23 23:14:27 +0200

    Made coordinate variables into vars.

--- src/vis/Pos.scala
+++ src/vis/Pos.scala
@@ -3,7 +3,7 @@ package vis
 import scala.math._
 
 // Represents a position in 3-dimensional space
-case class Pos(x: Double, y: Double, z: Double) {
+case class Pos(var x: Double, var y: Double, var z: Double) {
   // distance between two points
   def dist(p: Pos) = sqrt(pow(x - p.x, 2) + pow(y - p.y, 2) + pow(z - p.z, 2))
   // the distance between two points in the xy-plane
