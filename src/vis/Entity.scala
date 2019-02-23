160a3751e27943f16fb2f6276d6a14fab22ef16a
Markus <Markus@MARKUS-PC>
2019-02-23 21:12:24 +0200

    Added methods to Entity class that handle vertex calculations.

--- src/vis/Entity.scala
+++ src/vis/Entity.scala
@@ -1,7 +1,19 @@
 package vis
 
-case class Entity (pos: Pos) {
-  
+import scala.collection.mutable.Buffer
+import scala.math
+
+case class Entity(pos: Pos) {
+  var rot: Double = 0.0 // rotation in radians along z-axis
+  val verticies = Buffer[Pos]() // positions in relation to the entity's origin
+
+  // calculates a single point's location with z-axis rotation applied
+  def rotPos(p: Pos) = Pos(p.x*math.cos(rot) - p.y*math.sin(rot), p.x*math.sin(rot) + p.y*math.cos(rot), p.z)
+
+  def rotPositions = verticies.map(rotPos)
+
+  // vertex positions in global coordinate system
+  def vertexPositions = rotPositions.map(p => Pos(p.x + pos.x, p.y + pos.y, p.z + pos.z))
 }
 
 /*class Wall extends Entity {
