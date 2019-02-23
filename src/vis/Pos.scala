6045243832dac9352418f29725453c5c0bab9046
Markus <Markus@MARKUS-PC>
2019-02-23 23:38:43 +0200

    Added methods for relative position and rotating points along z-axis.

--- src/vis/Pos.scala
+++ src/vis/Pos.scala
@@ -2,12 +2,16 @@ package vis
 
 import scala.math._
 
-// Represents a position in 3-dimensional space
+/**Represents a position in 3-dimensional space*/
 case class Pos(var x: Double, var y: Double, var z: Double) {
-  // distance between two points
+  /**The distance between two points.*/
   def dist(p: Pos) = sqrt(pow(x - p.x, 2) + pow(y - p.y, 2) + pow(z - p.z, 2))
-  // the distance between two points in the xy-plane
+  /**The distance between two points in the xy-plane.*/
   def horDist(p: Pos) = sqrt(pow(x - p.x, 2) + pow(y - p.y, 2))
+  /**The relative position of a position compared to this one.*/
+  def relPos(p: Pos) = Pos(p.x - x, p.y - y, p.z - z)
+  /**Rotates the point along the origin and the z-axis*/
+  def rotated(rot: Double) = Pos(x*math.cos(rot) - y*math.sin(rot), x*math.sin(rot) + y*math.cos(rot), z)
   
   def apply(x: Double, y: Double, z: Double) = {
     this.x = x
