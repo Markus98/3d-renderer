f14b8d52cc27609a5d35cc3b87646961e578b095
Markus <Markus@MARKUS-PC>
2019-02-23 23:11:04 +0200

    Added method for adding walls to a stage. Made square width a constant.

--- src/vis/Stage.scala
+++ src/vis/Stage.scala
@@ -10,19 +10,35 @@ class Stage {
   /**The walls that are aligned with the x-axis.*/
   val horWalls = Map[(Int, Int), Entity]()
   
-  def position(x: Int, y: Int) = Pos(x, y, 0)
+  /**Get position in world from grid coordinates.*/
+  def position(x: Int, y: Int) = Pos(Stage.SqWidth*x, Stage.SqWidth*y, 0)
+  def wallPosition(x: Int, y: Int, dir: Dir) = Pos(Stage.SqWidth*(x + 0.5*dir.xMul), Stage.SqWidth*(y + 0.5*dir.xMul), 0)
   
   def getWall(x: Int, y: Int, dir: Dir) = dir match {
-    case Dir.North => horWalls.get(x, y+1)
-    case Dir.East => verWalls.get(x+1, y)
+    case Dir.North => horWalls.get(x, y + 1)
+    case Dir.East  => verWalls.get(x + 1, y)
     case Dir.South => horWalls.get(x, y)
-    case Dir.West => verWalls.get(x, y)
+    case Dir.West  => verWalls.get(x, y)
+  }
+
+  def addWall(x: Int, y: Int, dir: Dir): Boolean = {
+    if (this.getWall(x, y, dir).isEmpty) {
+      dir match {
+        case d @ Dir.North => horWalls((x, y + 1)) = new Wall(wallPosition(x, y, d), d)
+        case d @ Dir.East  => verWalls((x + 1, y)) = new Wall(wallPosition(x, y, d), d)
+        case d @ Dir.South => horWalls((x, y)) = new Wall(wallPosition(x, y, d), d)
+        case d @ Dir.West  => verWalls((x, y)) = new Wall(wallPosition(x, y, d), d)
+      }
+      true
+    } else false
   }
 }
 
-sealed abstract class Dir (val xDir: Int, val yDir: Int, val rot: Double) {
-  
+object Stage {
+  val SqWidth = 1.0 // The width of a single square.
 }
+
+sealed abstract class Dir (val xMul: Int, val yMul: Int, val rot: Double) 
 
 object Dir {
   case object North extends Dir(0, 1, 0)
