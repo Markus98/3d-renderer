bc16b89afd6ea47e8ae9687f6dd999b8e57d0aae
Markus <Markus@MARKUS-PC>
2019-02-23 23:16:22 +0200

    Added apply method.

--- src/vis/Pos.scala
+++ src/vis/Pos.scala
@@ -8,4 +8,10 @@ case class Pos(var x: Double, var y: Double, var z: Double) {
   def dist(p: Pos) = sqrt(pow(x - p.x, 2) + pow(y - p.y, 2) + pow(z - p.z, 2))
   // the distance between two points in the xy-plane
   def horDist(p: Pos) = sqrt(pow(x - p.x, 2) + pow(y - p.y, 2))
+  
+  def apply(x: Double, y: Double, z: Double) = {
+    this.x = x
+    this.y = y
+    this.z = z
+  }
 }
