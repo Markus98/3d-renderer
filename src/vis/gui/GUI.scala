6d7955ddba89aeff051965117cf329a9f34ce5ef
Markus <Markus@MARKUS-PC>
2019-02-28 23:40:08 +0200

    Added initial implementation of 3d-rendering.

--- src/vis/gui/GUI.scala
+++ src/vis/gui/GUI.scala
@@ -5,31 +5,49 @@ import scalafx.application.JFXApp
 import scalafx.scene.Scene
 import scalafx.scene.paint.Color._
 import scalafx.scene.shape.Polygon
-import scala.Vector
+import scala.collection.mutable.Buffer
+import vis._
 
-class GUI extends JFXApp {
+object GUI extends JFXApp {
+  var game = new Game()
+  game.player.rot = 0
+  game.stage.addWall(0, 2, Dir.East)
+  game.stage.addWall(0, 2, Dir.West)
+  game.stage.addWall(0, 2, Dir.North)
+  game.stage.addWall(1, 1, Dir.North)
+  game.stage.addWall(1, 1, Dir.East)
+  
   stage = new JFXApp.PrimaryStage {
-    title.value = "Hello Stage"
-    width = 600
-    height = 450
+    title.value = "3D Visualizator"
+    val w = 1280
+    val h = 720
+    width = w
+    height = h
     scene = new Scene {
-      fill = LightGreen
+      fill = White
+      content = game.stage.getWalls.map(drawEntity)
+    }
+    
+    def drawEntity(ent: Entity): Polygon = {
+      val rel = game.getRelEntity(ent)
+      val drawPoints = Buffer[java.lang.Double]()
       
-      content = new Polygon {
-        points ++= Vector(10, 10, 100, 100, 23, 45)
-        fill = Green
+      def calcPixel(p: Pos) = {
+        val multiplier = 2000
+        val x = w/2.0 + (p.x/p.y*Player.MinView) * multiplier
+        val y = h/2.0 + ((Player.Height - p.z)/p.y*Player.MinView) * multiplier
+        (x,y)
+      }
+      for (point <- rel.vertexPositions.map(calcPixel)) {
+        drawPoints += point._1
+        drawPoints += point._2
+      }
+      val col = 240 - (rel.pos.dist(Pos(0,0,0))*35).toInt
+      new Polygon() {
+        points ++= drawPoints
+        fill = rgb(col, col, col)
         stroke = Black
       }
-        
-      //Polygon(10,10,30,10,10,30,30,30)
-      
-      /*content = new Rectangle {
-        x = 25
-        y = 40
-        width = 100
-        height = 100
-        fill <== when(hover) choose Green otherwise Red
-      }*/
     }
   }
 }
