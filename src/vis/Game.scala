cdc5890925d36b9bb43ca0659514acdeed1f7deb
Markus <Markus@MARKUS-PC>
2019-02-28 20:10:38 +0200

    Added a method to be used for getting relative coordinates to camera.

--- src/vis/Game.scala
+++ src/vis/Game.scala
@@ -7,5 +7,12 @@ class Game {
   val stage = new Stage()
   val entities = Buffer[Entity]()
   
+  /**Returns a new instance of a given entity whose position and rotation have been made
+   * relative to the player's position and rotation.*/
+  def getRelEntity(ent: Entity): Entity = {
+    val relEntity = new Entity(player.pos.relPos(ent.pos).rotated(-player.rot), -player.rot)
+    relEntity.verticies ++= ent.verticies
+    relEntity
+  }
   
 }
