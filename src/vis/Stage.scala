973068ee414d2ec1c550f5b51cba5f1774a71dde
Markus <Markus@MARKUS-PC>
2019-03-23 00:10:02 +0200

    Moved constant type to declaration.

--- src/vis/Stage.scala
+++ src/vis/Stage.scala
@@ -38,7 +38,7 @@ class Stage {
 }
 
 object Stage {
-  val SqWidth = 1.0 // The width of a single square.
+  val SqWidth: Double = 1 // The width of a single square.
 }
 
 sealed abstract class Dir (val xMul: Int, val yMul: Int, val rot: Double) 
