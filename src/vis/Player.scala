c9aa928023c726434b02f08ef08c75b70b3941de
Markus <Markus@MARKUS-PC>
2019-04-03 21:01:27 +0300

    Added simple methods for moving player grid coordinates.

--- src/vis/Player.scala
+++ src/vis/Player.scala
@@ -1,8 +1,32 @@
 package vis
 
+import vis._
+
 class Player {
   val pos = Pos(0,0,0)
   var rot: Double = 0
+  /**Indicates whether the player is moving towards the desired position in a stage.*/
+  var moving = false
+  
+  //the player's desired position and direction in grid coordinates
+  var gridX: Int = 0
+  var gridY: Int = 0
+  var dir: Dir = Dir.North
+  
+  def moveForward() = {
+    gridX += dir.xMul
+    gridY += dir.yMul
+  }
+  
+  def moveBackward() = {
+    gridX -= dir.xMul
+    gridY -= dir.yMul
+  }
+  
+  def turnLeft() = dir = dir.antiClockwise
+  def turnRight() = dir = dir.clockwise
+  
+  
 }
 
 object Player {
